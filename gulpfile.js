728de2b0f8a56369700b11780be7f21abc898630
WooodHead <WooodHead@users.noreply.github.com>
2017-09-28 23:09:16 +0800

    change name

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,6 @@
 var gulp = require('gulp');
 var browserSync = require('browser-sync');
 var nodemon = require('gulp-nodemon');
-var ejsLint = require('ejs-lint');
 var sass = require('gulp-sass')
 
 var paths = {
@@ -21,28 +20,41 @@ gulp.task('sass', function () {
 gulp.task('watch', function () {
   gulp.watch([paths.sass], ['sass'])
   gulp.watch([paths.public + '/**/*.*'], browserSync.reload)
+
 })
 
 gulp.task('nodemon', function (cb) {
   var started = false;
   return nodemon({
-    script: 'bin/www'
-  }).on('start', function () {
-    if (!started) {
-      cb();
-      started = true;
-    }
-  });
+      script: 'bin/www'
+    }).on('start', function () {
+      if (!started) {
+        cb();
+        started = true;
+      }
+    })
+    .on('restart', function () {
+      console.log('restarted!')
+      setTimeout(function () {
+        browserSync.reload();
+      }, 1000);
+    })
+    .on('crash', function () {
+      console.error('Application has crashed!\n')
+      stream.emit('restart', 10) // restart the server in 10 seconds 
+    });
 });
 
 
 gulp.task('browser-sync', ['nodemon', 'watch'], function () {
   browserSync.init(null, {
     proxy: "http://localhost:8080",
-    files: ['public/**/*.*', 'views/**/*.*', 'controller/**/*.*', 'model/**/*.*', 'routes/**/.*.*', 'app/**/*.*'],
+    files: ['serve-index/**/*.*', 'public/**/*.*', 'views/**/*.*', 'controller/**/*.*', 'model/**/*.*', 'routes/**/.*.*', 'app/**/*.*'],
+    // files: ['serve-index/**/*.*', 'public/**/*.*', 'views/**/*.*', 'controller/**/*.*', 'model/**/*.*', 'routes/**/.*.*', 'app/**/*.*'],
     browser: "google chrome",
     port: 7000,
-    open: false
+    open: false,
+    online: true
   });
 });
 
